11b36d5dc9ef8840b4a910cb218765a15d9afb40
daoxuantri <thegioithangkhung@gmail.com>
2024-11-09 03:47:19 +0700

    upload productdetail ( model , controller)

---
+++ routes/productdetails.routes.js
@@ -0,0 +1,10 @@
+const productdetailController = require("../controllers/productdetails.controller");
+const express = require("express");
+const router = express.Router();
+
+
+router.get("/:id",  productdetailController.getbyid);
+router.post("/create",  productdetailController.createdetailproduct);
+
+
+module.exports = router;    
