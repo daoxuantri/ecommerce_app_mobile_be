f4ec5b7562a5584d7c634d17c89490be4f25e318
daoxuantri <thegioithangkhung@gmail.com>
2024-11-26 18:03:38 +0700

    upload initial , discount field

--- models/variants.js
+++ models/variants.js
@@ -1,22 +1,28 @@
 const mongoose = require("mongoose");
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
-const variantSchema = new Schema ({
+const variantSchema = new Schema(
+  {
     product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
-        default : null
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      default: null,
     },
     memory: {
-        type: String, 
-      },
+      type: String,
+    },
     variants: [
-        {
-          color: { type: String, required: true }, 
-          price: { type: Number, required: true }, 
+      {
+        color: { type: String, required: true },
+        price: {
+          initial: { type: Number, required: true }, 
+          discount: { type: Number, default: null }, 
         },
+      },
     ],
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
-const VariantProduct = mongoose.model("VariantProduct",variantSchema);
+const VariantProduct = mongoose.model("VariantProduct", variantSchema);
 module.exports = VariantProduct;
