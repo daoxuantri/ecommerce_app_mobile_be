9a984e13bad343991823d7052aad30e2cccb5125
ViKa <nvkhang191l@gmail.com>
2024-12-06 20:28:52 +0700

    add delete adress api

--- routes/address.routes.js
+++ routes/address.routes.js
@@ -6,5 +6,5 @@ const router = express.Router();
 router.post("/create",  addressController.createaddress);
 router.get("/:idUser",  addressController.getalladdressbyuser);
 router.get("/:idUser/getdefault",  addressController.getaddressdefault);
-
+router.delete("/:id", addressController.deleteAddress);
 module.exports = router;    
